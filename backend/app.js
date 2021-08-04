142fc85344d4dee2bfa107bf8cad4494d9807113
leonhardt <jacob.leonhardt649@gmail.com>
2021-08-04 12:20:36 -0500

    add middlware

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+// Set Requirements
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const csurf = require('csurf')
+const helmet = require('helmet')
+const cookieParser = require('cookie-parser')
+
+// Check Current Environment
+const { environment } = require('./config')
+const isProduction = environment === 'production'
+
+// Initialize App
+const app = express()
+
+app.use(morgan('dev'))
+app.use(cookieParser())
+app.use(express.json())
+
+// Security Middleware
+if (!isProduction) {
+    app.use(cors());
+}
+
+app.use(helmet({
+    contentSecurityPolicy: false
+}))
+
+app.use(csurf({
+    cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true,
+    }
+}))
