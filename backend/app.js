e9f902df778af9bb637eabca2b2cf16273e3dd72
leonhardt <jacob.leonhardt649@gmail.com>
2021-08-04 19:52:11 -0500

    error handling set up

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,16 @@
-// Set Requirements
+// Set Requirements/Imports
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const csurf = require('csurf')
 const helmet = require('helmet')
 const cookieParser = require('cookie-parser')
-
-// Set Imports
+const { ValidationError } = require('sequelize')
 const routes = require('./routes')
 
 // Check Current Environment
 const { environment } = require('./config')
+const { restart } = require('nodemon')
 const isProduction = environment === 'production'
 
 // Initialize App
@@ -38,5 +38,40 @@ app.use(csurf({
 }))
 
 app.use(routes)
+
+// ERROR HANDLING
+// Catch unhandled requests and forward to handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    // invoking next with an arg means other err handlers will be invoked as well
+    next(err)
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+      err.errors = err.errors.map((e) => e.message);
+      err.title = 'Validation error';
+    }
+    next(err);
+  });
+
+// Error Formatting
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack,
+    });
+  });
+
+
 
 module.exports = app
