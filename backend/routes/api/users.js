99a5a45c601e81e4003f9a1ca62242fef7e624fd
leonhardt <jacob.leonhardt649@gmail.com>
2021-08-05 15:56:06 -0500

    phase one complete

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -8,11 +8,9 @@ const { handleValidationErrors } = require('../../utils/validation')
 
 const validateSignup = [
     check('email')
-      .exists({ checkFalsy: true })
       .isEmail()
       .withMessage('Please provide a valid email.'),
     check('username')
-      .exists({ checkFalsy: true })
       .isLength({ min: 4 })
       .withMessage('Please provide a username with at least 4 characters.'),
     check('username')
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,10 +5,5 @@ const apiRouter = require('./api')
 // use apiRouter whenever there is a path with '/api'
 router.use('/api', apiRouter)
 
-router.get('/home', function(req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken())
-    res.send('IT\'S WORKING!')
-})
-
 
 module.exports = router;
