c00c416d857b33de79bc87c68e4e8186d80e01b7
leonhardt <jacob.leonhardt649@gmail.com>
2021-08-07 12:16:13 -0500

    add navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Nav from './components/Navigation/index'
 import Login from './components/Login';
 import Signup from './components/Signup'
 
 function App() {
   return (
-    <Switch>
-      <Route path="/login" exact>
-        <Login />
-      </Route>
-      <Route path="/signup" exact>
-        <Signup />
-      </Route>
-    </Switch>
+    <>
+      <Nav />
+      <Switch>
+        <Route path="/login" exact>
+          <Login />
+        </Route>
+        <Route path="/signup" exact>
+         <Signup />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,11 +1,29 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Navlink } from 'react-redux'
+import ProfileButton from './profilebutton'
+import Logo from '../../../public/vertical_explorer_logo-transparent.png'
 import 'nav.css'
 
+
 const Nav = () => {
+
+    const user = useState(state => state.session.user)
+
     return (
         <div id='navbar'>
-
+            <div className="right">
+                <Navlink to="/">{Logo}</Navlink>
+            </div>
+            <div className="left">
+            {user ?
+            <>
+                <Navlink>Climbs</Navlink>
+                <ProfileButton />
+            </> :
+            <>
+                <Navlink>Login</Navlink>
+                <Navlink>Signup</Navlink>
+            </>}
         </div>
     )
 }
